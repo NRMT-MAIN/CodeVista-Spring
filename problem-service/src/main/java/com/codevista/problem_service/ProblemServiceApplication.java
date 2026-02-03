13e250eb031553bad294133b04c7b8e4291d4cd2
NRMT-MAIN <sahunirmit.main@gmail.com>
2026-02-03 19:50:46 +0530

    Problem Service Setup

---
+++ problem-service/src/main/java/com/codevista/problem_service/ProblemServiceApplication.java
@@ -0,0 +1,21 @@
+package com.codevista.problem_service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemServiceApplication {
+
+	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load() ;
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey() , entry.getValue()) ;
+        });
+
+		SpringApplication.run(ProblemServiceApplication.class, args);
+	}
+
+}
